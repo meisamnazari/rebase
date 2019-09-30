30f22287a7f514a3bc6741dff48c20f5511ed0ef
meisam <meisamnazari71@gmail.com>
2019-09-30 11:48:35 +0330

    save

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-           Kireeeeee kharrrrr
+           React
         </a>
       </header>
     </div>
