9949abc935fe33ebca98cf4c15cb2b457cbe0146
meisam <meisamnazari71@gmail.com>
2019-09-30 11:49:10 +0330

    save/master

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn
+          Kire khaaaaaaaaaaarrrrrrrrrrrrrrrrrrr
         </a>
       </header>
     </div>
