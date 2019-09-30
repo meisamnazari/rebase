11b7bce3a0ab9e27f27cdb66bff2bafb7af7f92e
meisam <meisamnazari71@gmail.com>
2019-09-30 11:46:21 +0330

    romeve/everything

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-           React
+           Kireeeeee kharrrrr
         </a>
       </header>
     </div>
